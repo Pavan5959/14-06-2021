0a26dcca1d0ee391e1f4c96b771adca7d0451325
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-14 22:54:46 +0530

    Create return.js

---
+++ iterators/return.js
@@ -0,0 +1,18 @@
+function* gen(end)
+{
+   for(let i=0; i<end; i++){
+     try{
+     yield i;
+     }
+     catch(e){
+       console.log("error found")
+     }
+   } 
+}
+
+let it = gen(3);
+
+console.log(it.next());
+console.log(it.return('error occured'));
+console.log(it.next());
+console.log(it.next());
